22ef0972f80a9b60ce705f91da1a18352bcfadcc
Mian Muhammad Sharjeel Safdar <miansharjeelsafdar@gmail.com>
2021-06-30 06:22:40 +0500

    Added frontend-deploy stack and tests for it

--- backend/test/p14b-frontend-deploy-stack.test.ts
+++ backend/test/p14b-frontend-deploy-stack.test.ts
@@ -1,16 +1,22 @@
-import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
-import * as cdk from '@aws-cdk/core';
-import * as P14bFrontendDeploy from '../lib/p14b-frontend-deploy-stack';
+import { expect as expectCDK, haveResource } from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as P14bFrontendDeploy from "../lib/p14b-frontend-deploy-stack";
 
 const createTestStack = (app: cdk.App) =>
   new P14bFrontendDeploy.FrontendDeployStack(app, "MyTestStack");
 
-test('Empty Stack', () => {
-    const app = new cdk.App();
-    // WHEN
-    const stack = createTestStack(app);
-    // THEN
-    expectCDK(stack).to(matchTemplate({
-      "Resources": {}
-    }, MatchStyle.EXACT));
+test("Stack has a S3 Bucket", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = createTestStack(app);
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+});
+
+test("Stack has a CloudFront Distribution", () => {
+  const app = new cdk.App();
+  // WHEN
+  const stack = createTestStack(app);
+  // THEN
+  expectCDK(stack).to(haveResource("AWS::CloudFront::Distribution"));
 });
